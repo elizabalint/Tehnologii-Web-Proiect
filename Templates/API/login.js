5d0615c58a7e1d5ac126302e7521a2eff64d9cc8
ignatrovino12 <vlad.rovin@yahoo.com>
2023-06-20 14:17:19 +0300

    made the remember me button to work properly

--- Templates/API/login.js
+++ Templates/API/login.js
@@ -11,15 +11,27 @@ function showErrorWithTimeout(message, timeout) {
 }
 
 const loginForm = document.getElementById('loginForm');
+const usernameInput = document.getElementById('username');
+const rememberCheckbox = document.getElementById('rememberCheckbox');
+
+//If the username was remembered
+if (localStorage.getItem('remembered') === 'true') {
+  rememberCheckbox.checked = true;
+  usernameInput.value = localStorage.getItem('rememberedUsername');
+}
+
+//if you are in a session you can't access this
+//if (document.cookie) location.replace("General.html");
 
 
+//login form 
 if (loginForm) {
   loginForm.addEventListener('submit', (event) => {
     event.preventDefault(); // Prevent the default form submission
 
     const formData = new FormData(loginForm);
     const url = 'http://localhost:8081/api/login'; // API URL
-
+    const username = usernameInput.value;
 
     fetch(url, {
       method: 'POST',
@@ -30,21 +42,32 @@ if (loginForm) {
 
 
         if (data.success) {
-          // If login was successful      
-          
-        document.cookie = "session="+data.message; 
-        window.location.href = "General.html";
-          //alert(document.cookie);        
-         // location.replace("General.html");
-         //console.log(data.message);
-         
+          // If login was successful  
+
+         // Check if the "Remember me" checkbox is checked
+          if (rememberCheckbox.checked) {
+            // Store the username and the "remembered" flag in localStorage
+            localStorage.setItem('rememberedUsername', username);
+            localStorage.setItem('remembered', 'true');
+
+          } else {
+            // Clear the stored username and the "remembered" flag from localStorage
+            localStorage.removeItem('rememberedUsername');
+            localStorage.removeItem('remembered');
+
+          }
+
+          //create session cookie and go to general.html page
+          document.cookie = "session=" + data.message;
+          window.location.href = "General.html";
+
 
         } else {
           // If login failed
 
           console.log('Login failed');
           console.log('Error message:', data.message);
-          
+
           showErrorWithTimeout(data.message, 3000);
 
         }
@@ -55,6 +78,7 @@ if (loginForm) {
       });
   });
 
+  // loginForm.reset();
 }
 
 
